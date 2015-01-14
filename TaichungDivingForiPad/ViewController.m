7d6f6a96f8ef888033e3fbfee5f79915af3fdf58
SamuelTeng <tcfsh@hotmail.com>
2015-01-14 10:26:38 +0800

    Initial Commit

---
+++ TaichungDivingForiPad/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TaichungDivingForiPad
+//
+//  Created by Samuel Teng on 2015/1/14.
+//  Copyright (c) 2015年 Samuel Teng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
